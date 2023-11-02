9f5649c3dd9238ab85b41b4cd5df5ded89f21733
tefgo <tefgo@DESKTOP-QN9FHUR>
2023-11-02 11:13:13 +0100

    Updated Database

---
+++ TelegramBot/Migrations/20231102101237_Init.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Warn = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMedals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    MedalId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMedals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMedals_Medals_MedalId",
+                        column: x => x.MedalId,
+                        principalTable: "Medals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMedals_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medals",
+                columns: new[] { "Id", "ImagePath", "Name" },
+                values: new object[,]
+                {
+                    { 1, "C:\\Users\\tefgo\\source\\repos\\FuhrerMonaruBot\\FuhrerMonaruBot\\images\\braz.jpg", "Bronze" },
+                    { 2, "C:\\Users\\tefgo\\source\\repos\\FuhrerMonaruBot\\FuhrerMonaruBot\\images\\srebro.jpg", "Silver" },
+                    { 3, "C:\\Users\\tefgo\\source\\repos\\FuhrerMonaruBot\\FuhrerMonaruBot\\images\\zloto.jpg", "Gold" },
+                    { 4, "C:\\Users\\tefgo\\source\\repos\\FuhrerMonaruBot\\FuhrerMonaruBot\\images\\karny.jpg", "Warn" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMedals_MedalId",
+                table: "UserMedals",
+                column: "MedalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMedals_UserId",
+                table: "UserMedals",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserMedals");
+
+            migrationBuilder.DropTable(
+                name: "Medals");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Net;
-//using Telegram.Bot;
-//using Telegram.Bot.Exceptions;
-//using Telegram.Bot.Polling;
-//using Telegram.Bot.Types;
-//using Telegram.Bot.Types.Enums;
-//using Telegram.Bot.Types.ReplyMarkups;
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using TelegramBot;
 
+Console.WriteLine("a");
